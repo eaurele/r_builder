56418cf0bd65ba6e5af91aadb5f7f7255c69138a
Aurele <aurele@saagie.com>
2016-08-26 16:54:37 +0200

    fix: Check should happen one level up the package dir
    Also split to multiline strings and multiline commands in the same entrypoint.sh script

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,18 @@
 cd /project
 
-Rscript -e 'deps <- devtools::dev_package_deps(dependencies = NA);devtools::install_deps(dependencies = TRUE);if (!all(deps$package %in% installed.packages())) { message("missing: ", paste(setdiff(deps$package, installed.packages()), collapse=", ")); q(status = 1, save = "no")}'
+Rscript -e 'deps <- devtools::dev_package_deps(dependencies = NA);
+  devtools::install_deps(dependencies = TRUE);
+  if (!all(deps$package %in% installed.packages())) { 
+  message("missing: ", paste(setdiff(deps$package, 
+  installed.packages()), collapse=", ")); 
+  q(status = 1, save = "no")}'
 
+# Build happens in the package directory
 R CMD build  .
 
-R CMD check *.tar.gz --as-cran --no-manual --no-manual --no-manual --no-manual; 
+# Check happens one level up, hence "../"
+# Note that *.tar.gz is risky
+R CMD check ../*.tar.gz --as-cran --no-manual \
+  --no-manual --no-manual --no-manual; 
 
 Rscript -e 'covr::package_coverage()'
